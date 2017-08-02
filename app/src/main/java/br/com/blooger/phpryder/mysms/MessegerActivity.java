11b1155b17e055dd8a24303b19b91b6951e5620f
Carlos <anjoazrael10@gmail.com>
2017-08-01 21:21:54 -0300

    add files e path android

---
+++ app/src/main/java/br/com/blooger/phpryder/mysms/MessegerActivity.java
@@ -0,0 +1,13 @@
+package br.com.blooger.phpryder.mysms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MessegerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messeger);
+    }
+}
